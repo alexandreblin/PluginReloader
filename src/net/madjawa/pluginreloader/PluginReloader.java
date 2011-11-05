b79c1d81841a9d9687e9a138a0c4a50a876ecb2f
Jon la Cour <jon@lacour.me>
2011-11-04 20:35:43 -0600

    Fixed the loading of permissions. Removed useless log messages for when there were no permissions no load/unload.

--- src/net/madjawa/pluginreloader/PluginReloader.java
+++ src/net/madjawa/pluginreloader/PluginReloader.java
@@ -204,7 +204,7 @@ public class PluginReloader extends JavaPlugin {
 						manager.removePermission(p.next().toString());
 					}
 				} catch (NoSuchMethodError e) {
-					log.info("[PluginReloader] " + pluginName + " has no permissions to unload.");
+					// Do nothing
 				}
 				
 				// ta-da! we're done (hopefully)
@@ -225,14 +225,14 @@ public class PluginReloader extends JavaPlugin {
 		
 		manager.enablePlugin(plugin);
 		
+		List<Permission> permissionlist = plugin.getDescription().getPermissions();
+		Iterator p = permissionlist.iterator();
 		try {
-			List<Permission> permissionlist = plugin.getDescription().getPermissions();
-			Iterator p = permissionlist.iterator();
 			while (p.hasNext()) {
-				manager.removePermission(p.next().toString());
+				manager.addPermission(p.next());
 			}
 		} catch (NoSuchMethodError e) {
-			log.info("[PluginReloader] " + pluginName + " has no permissions to load.");
+			// Do nothing
 		}
 	}
 }
